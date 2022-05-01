7e921093aeba5668760b62e65c6490146936ed89
Oncu <oohancan@gmail.com>
2022-05-01 20:25:50 +0300

    Initial Commit

---
+++ FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by Oncu Ohancan on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
