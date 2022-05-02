ee77576583cb852e9f0116ba7197defe6d4ce749
Oncu <oohancan@gmail.com>
2022-05-02 22:35:23 +0300

    add asset image

---
+++ FoodDeliveryApp/Views/FoodItem.swift
@@ -0,0 +1,20 @@
+//
+//  FoodItem.swift
+//  FoodDeliveryApp
+//
+//  Created by Oncu Ohancan on 2.05.2022.
+//
+
+import SwiftUI
+
+struct FoodItem: View {
+    var body: some View {
+        Image("lasagna")
+    }
+}
+
+struct FoodItem_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodItem()
+    }
+}
