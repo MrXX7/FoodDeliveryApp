81d0ecf0a2ba9571f3b5827e4a54c88ac7edd742
Oncu <oohancan@gmail.com>
2022-05-03 20:58:03 +0300

    add categories and list to foodlist

--- FoodDeliveryApp/Views/FoodList.swift
+++ FoodDeliveryApp/Views/FoodList.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct FoodList: View {
+    
+    var categories:[String:[Food]] {
+        .init(
+            grouping: Menu,
+            by: {$0.kitchen.self}
+        )
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List(categories.keys.sorted(), id:
+                    \String.self){ key in
+                
+                FoodRow(kitchenName: "\(key) Food",
+                    foods: self.categories[key]!)
+                .frame(height: 300)
+                .padding(.top)
+                .padding(.bottom)
+                
+            } .navigationBarTitle("Food Delivery")
+        }
     }
 }
 
